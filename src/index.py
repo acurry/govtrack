749b29f6092cfb781958653bb728681ec5c7a592
Andrew Curry <acurry@dragos.com>
2019-11-06 11:59:08 -0500

    update main script

--- src/index.py
+++ src/index.py
@@ -9,11 +9,15 @@ DATE_FORMAT = '%Y-%m-%d'
 ending_date = date.today()
 starting_date = date(2018, 10, 1)
 
-data_url = BASE_URL + starting_date.strftime(date_format) + '/' + ending_date.strftime(date_format)
+data_url = BASE_URL + starting_date.strftime(DATE_FORMAT) + '/' + ending_date.strftime(DATE_FORMAT)
 
-response = requests.get(get_url)
+response = requests.get(data_url)
 pruned_response_text = response.text[5:-1]
 parsed_json = json.loads(pruned_response_text)
 
-clean_data = [{ k: v.strip() for (k, v) in item.items()} for item in parsed_json]
+clean_data = [{ k: v.strip() if v else '' for (k, v) in item.items()} for item in parsed_json]
+
+sorted_clean_data = sorted(clean_data, key = lambda i: i['submitDate'], reverse=True)
+
+pprint.pprint(sorted_clean_data[:5])
 
